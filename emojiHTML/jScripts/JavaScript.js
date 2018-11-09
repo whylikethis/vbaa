d7f910df2b64b2def5782c524895a638060418dd
whylikethis <waylikethis@gmail.com>
2018-11-09 20:51:03 +0200

    Add files via upload

---
+++ emojiHTML/jScripts/JavaScript.js
@@ -0,0 +1,7 @@
+﻿function testclick(event){
+	
+	document.getElementById("LastClicked").innerHTML = "1.png";
+	document.title = 'My new title here - 1.png';
+	 var clickedElement = event.currentTarget.alt;
+	 console.log(clickedElement);
+}
